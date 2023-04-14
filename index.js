02266c45cc751d5feea64ef1557a6a20bd91531e
lokesh-wagh <129964049+lokesh-wagh@users.noreply.github.com>
2023-04-14 14:35:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var button = document.querySelector("#button");
+button.addEventListener("click",function(){
+    var a=Math.floor(Math.random()*6+1);
+var b=Math.floor(Math.random()*6+1);
+var image1=document.querySelector("#image1");
+var image2=document.querySelector("#image2");
+var heading=document.querySelector("#heading");
+
+image1.setAttribute("src","/images/dice"+a.toString()+".png");
+image2.setAttribute("src","/images/dice"+b.toString()+".png");
+if(a>b){
+heading.textContent="Player One Wins";
+
+}
+else{
+    if(a<b){
+    heading.textContent="Player Two Wins"; 
+}
+else{
+    heading.textContent="It's a Draw";
+}}
+
+
+})
+var button1=document.querySelector("#button1");
+button1.addEventListener("click",function(){
+    var image1=document.querySelector("#image1");
+    var image2=document.querySelector("#image2");
+    image1.setAttribute("src","/images/dice6.png");
+    image2.setAttribute("src","/images/dice6.png");
+    var heading=document.querySelector("#heading");
+    heading.textContent="Roll dice";
+})

8d34be62f95ab2c16677f48c607a775bed04d784
lokesh-wagh <129964049+lokesh-wagh@users.noreply.github.com>
2023-04-14 14:36:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var image1=document.querySelector("#image1");
 var image2=document.querySelector("#image2");
 var heading=document.querySelector("#heading");
 
-image1.setAttribute("src","/images/dice"+a.toString()+".png");
-image2.setAttribute("src","/images/dice"+b.toString()+".png");
+image1.setAttribute("src","/dice"+a.toString()+".png");
+image2.setAttribute("src","/dice"+b.toString()+".png");
 if(a>b){
 heading.textContent="Player One Wins";
 
@@ -26,8 +26,8 @@ var button1=document.querySelector("#button1");
 button1.addEventListener("click",function(){
     var image1=document.querySelector("#image1");
     var image2=document.querySelector("#image2");
-    image1.setAttribute("src","/images/dice6.png");
-    image2.setAttribute("src","/images/dice6.png");
+    image1.setAttribute("src","/dice6.png");
+    image2.setAttribute("src","/dice6.png");
     var heading=document.querySelector("#heading");
     heading.textContent="Roll dice";
 })
